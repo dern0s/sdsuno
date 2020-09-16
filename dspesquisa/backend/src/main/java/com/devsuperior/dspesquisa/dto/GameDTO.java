d10456d30b3be0f8f6f21fd9b80bebcddad5eda9
Denis Edrey <denis.alves.hu@gmail.com>
2020-09-16 20:09:49 -0300

    services and DTO

---
+++ dspesquisa/backend/src/main/java/com/devsuperior/dspesquisa/dto/GameDTO.java
@@ -0,0 +1,46 @@
+package com.devsuperior.dspesquisa.dto;
+
+import com.devsuperior.dspesquisa.entities.Game;
+import com.devsuperior.dspesquisa.entities.enums.Platform;
+
+public class GameDTO {
+
+	private Long id;
+	private String title;
+	private Platform platform;
+	
+	public GameDTO() {}
+
+	public GameDTO(Game entity) {
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.platform = entity.getPlatform();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Platform getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(Platform platform) {
+		this.platform = platform;
+	}
+	
+	
+	
+}
