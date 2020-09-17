05f5957bb020d667b267e8da04b19628dd4931ba
Denis Edrey <denis.alves.hu@gmail.com>
2020-09-17 20:51:39 -0300

    After some clean up on the project structure

--- dspesquisa/front-web/src/App.tsx
+++ dspesquisa/front-web/src/App.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, mano!
-        </p>
-       
-      </header>
-    </div>
+    <h1>Hello, semana devSuperior!</h1>
   );
 }
 
