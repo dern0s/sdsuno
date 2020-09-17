bfb0b96389b9ae77f921e011d95c4e67c7e58de6
Denis Edrey <denis.alves.hu@gmail.com>
2020-09-17 12:51:50 -0300

    Implementing get records

--- dspesquisa/backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
+++ dspesquisa/backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -3,7 +3,7 @@ package com.devsuperior.dspesquisa.repositories;
 import java.time.Instant;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,8 +13,10 @@ import com.devsuperior.dspesquisa.entities.Record;
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
-	@Query("SELECT o FROM Record o")
-	Page<Record> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest);
+	@Query("SELECT obj FROM Record obj WHERE "
+			+ "(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND "
+			+ "(coalesce(:max, null) IS NULL OR obj.moment <= :max)")
+	Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
 
 	
 	
