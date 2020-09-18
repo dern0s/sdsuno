0ebba94ea1aa9e277aa47e77a70dad6d87373282
Denis Edrey <denis.alves.hu@gmail.com>
2020-09-18 19:40:52 -0300

    Adding Header Component

--- dspesquisa/front-web/src/App.tsx
+++ dspesquisa/front-web/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/';
+
 
 function App() {
   return (
-    <h1>Hello, semana devSuperior!</h1>
+    <div>
+      <Header/>
+    </div>
   );
 }
 
