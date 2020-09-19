01b89d9d89d674f2cd622dff62f6a9ef3a7dedbb
Denis Edrey <denis.alves.hu@gmail.com>
2020-09-19 17:21:46 -0300

    Created graphs

--- dspesquisa/front-web/src/pages/Records/index.tsx
+++ dspesquisa/front-web/src/pages/Records/index.tsx
@@ -4,7 +4,7 @@ import './styles.css';
 import { RecordsResponse } from './types';
 import { formatDate } from './helpers';
 import Pagination from './Pagination';
-import { Link } from 'react-router-dom';
+import Filters from '../../components/Filters';
 
 const BASE_URL : string = 'http://localhost:8080';
 
@@ -24,13 +24,7 @@ const Records = () => {
     return (
 
         <div className="page-container">
-            <div className="filters-container records-actions">
-                <Link to="/charts">
-                    <button className="action-filters">
-                        VER GRÁFICOS
-                    </button>
-                </Link>
-            </div>
+            <Filters link="/charts" linkText="VER GRÁFICOS" />
             <table className="records-table" cellPadding="0" cellSpacing="0">
                 <thead>
                     <tr>
