d700fa931a7c294a6c15bc09dcd64104a3ab847b
Denis Edrey <denis.alves.hu@gmail.com>
2020-09-18 21:48:53 -0300

    Finished Home Component

---
+++ dspesquisa/front-web/src/pages/Home.tsx/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './styles.css';
+import { ReactComponent as ArrowIcon } from '../../assets/Arrow.svg';
+import { ReactComponent as GamerIcon } from '../../assets/gamer.svg';
+
+const Home = () => (
+
+    <div className="home-container">
+        <div className="home-text">
+            <h1 className="home-text-title">
+                Quais jogos as pessoa gosta mais?
+            </h1>
+            <h3 className="home-text-subtitle">
+                Clique no botão abaixo e saiba quais jogos x galero está escolhendo!
+            </h3>
+            <div className="home-actions">
+                <button className="home-btn">
+                    PRECISO SABER!!!1ONZE
+                </button>
+                <div className="home-btn-icon">
+                    <ArrowIcon />
+                </div>
+            </div>
+        </div>
+
+        <GamerIcon className="home-image"/>
+
+    </div>
+
+);
+
+export default Home;
